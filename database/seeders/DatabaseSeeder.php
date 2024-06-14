0286ddf511744e545884f772f632f1e49d4416fc
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-14 17:05:28 +0200

    [add]seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            HouseTableSeeder::class,
+            ServiceSeeder::class,
+            SponsorTableseeder::class,
+
         ]);
     }
 }
