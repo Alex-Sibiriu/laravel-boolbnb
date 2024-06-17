3a358498bbc451147ba736b493f33fe49d036be6
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-17 17:50:34 +0200

    fix color

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -2,13 +2,13 @@
     <nav>
         <ul>
             <li class="my-4">
-                <a class="text-decoration-none text-white" href="{{ route('admin.home') }}">
+                <a class="text-decoration-none" href="{{ route('admin.home') }}">
                     <i class="fa-solid fa-house me-2"></i>
                     Home
                 </a>
             </li>
             <li class="my-4">
-                <a class="text-decoration-none text-white" href="{{ route('admin.houses.create') }}">
+                <a class="text-decoration-none" href="{{ route('admin.houses.create') }}">
                     <i class="fa-solid fa-circle-plus me-2"></i>
                     Nuovo castello
                 </a>
