1b5a73c9f73957729c0219628a7605ed82d9c446
EmilianaManzo <emiliana.ma96@gmail.com>
2024-06-17 15:12:54 +0200

    [add] searchbar, btn plus per nuove houses, funzione orderby

--- resources/views/admin/houses/index.blade.php
+++ resources/views/admin/houses/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="row pt-2 pb-5 px-5">
-
+  <div class="row py-2 px-5 justify-content-between">
     <div class="col-12">
+        {{-- alert di avvenuta cancellazione  --}}
         @if(session('deleted'))
         <div class="alert alert-success" role="alert">
         {{ session('deleted')}}
@@ -11,6 +11,19 @@
         @endif
     </div>
 
+    <div class="col-4 my-3">
+        <form action="{{route('admin.houses.index')}}" method="GET" class="d-flex" role="search">
+            <input name="toSearch" class=" toSearch form-control me-2" type="search" placeholder="Cerca" aria-label="Search">
+
+            <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-circle-chevron-right"></i></button>
+          </form>
+    </div>
+    <div class="col-4 my-3 text-end">
+        <a href="{{route('admin.houses.create')}}" class="btn btn-primary"><i class="fa-solid fa-plus"></i></a>
+    </div>
+  </div>
+
+  <div class="row pt-2 pb-5 px-5">
     <div class="col-12">
       <div class="px-2 bg-dark rounded-3 pb-1">
         <h2 class="py-3 text-white rounded-3 fw-bold fs-2 p-3 mt-3">I tuoi Castelli</h2>
@@ -19,9 +32,9 @@
           @if (count($houses) > 0)
             <thead>
               <tr>
-                <th class="ps-3 id-column" scope="col">ID</th>
-                <th scope="col">>Immagine</th>
-                <th scope="col">Nome</th>
+                <th class="ps-3 id-column" scope="col"><a href="{{route('admin.orderby', ['direction' => $direction , 'column'=> 'id'])}}">ID</a></th>
+                <th scope="col">Immagine</th>
+                <th scope="col"><a href="{{route('admin.orderby', ['direction' => $direction , 'column' => 'title'])}}">Nome</a></th>
                 <th scope="col">Sponsor</th>
                 <th scope="col">Pubblica</th>
                 <th class="text-center" scope="col">Azioni</th>
