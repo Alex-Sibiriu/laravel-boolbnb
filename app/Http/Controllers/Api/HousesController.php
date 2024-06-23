5f728b2b1784a3e2138c9b5a9da17f0722a7efc4
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-06-23 18:18:11 +0200

    [fix] index api method

--- app/Http/Controllers/Api/HousesController.php
+++ app/Http/Controllers/Api/HousesController.php
@@ -14,47 +14,9 @@ class HousesController extends Controller
 {
     public function index()
     {
-        $houses = House::with('user', 'messages', 'images', 'services', 'sponsors')->paginate(12);
+        $houses = House::with('user', 'messages', 'images', 'services', 'sponsors')->get();
         return response()->json($houses);
     }
-
-    // public function getNearbyCastles($address, $radius = 100)
-    // {
-    //     // Chiamata all'API di TomTom per ottenere le coordinate geografiche
-    //     $cacertPath = env('CACERT_PEM_PATH');
-    //     $client = new Client([
-    //         'base_uri' => 'https://api.tomtom.com/',
-    //         'verify' => $cacertPath,
-    //     ]);
-
-    //     $response = $client->get('search/2/geocode/' . $address . '.json', [
-    //         'query' => [
-    //             'key' => env('TOMTOM_API_KEY')
-    //         ]
-    //     ]);
-
-    //     $data = json_decode($response->getBody(), true);
-
-    //     if (empty($data['results'])) {
-    //         return response()->json(['error' => 'Address not found'], 404);
-    //     }
-
-    //     $position = $data['results'][0]['position'];
-    //     $latitude = $position['lat'];
-    //     $longitude = $position['lon'];
-
-    //     // Cerco i castelli entro il range specificato
-    //     $houses = House::selectRaw(
-    //         "*, ( 6371 * acos( cos( radians(?) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(?) ) + sin( radians(?) ) * sin( radians( latitude ) ) ) ) AS distance",
-    //         [$latitude, $longitude, $latitude]
-    //     )
-    //         ->having('distance', '<', $radius)
-    //         ->with('user', 'messages', 'images', 'services', 'sponsors')
-    //         ->orderBy('distance')
-    //         ->get();
-
-    //     return response()->json($houses);
-    // }
 
     public function getNearbyCastles(Request $request)
     {
@@ -142,8 +104,6 @@ class HousesController extends Controller
 
         return response()->json($houses);
     }
-
-
 
     public function getServices()
     {
