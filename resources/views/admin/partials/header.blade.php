9dfe49d933eb671a5459a2e33b6c44ded723fb14
EmilianaManzo <emiliana.ma96@gmail.com>
2024-06-26 16:57:19 +0200

    [add] dropdown

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,9 +1,9 @@
 <header>
 
-  <nav class="navbar navbar-expand-lg bg-body-tertiary header-admin">
+  <nav class="navbar navbar-expand bg-body-tertiary header-admin">
     <div class="container-fluid">
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class=" navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
           <li class="nav-item">
@@ -15,10 +15,24 @@
 
         </ul>
 
-        <ul class="navbar-nav mb-2 mb-lg-0">
-          <li class="nav-item text-capitalize">
-            <span class="nav-link text-white fw-bold user">
+        <ul class="navbar-nav mb-2 mb-lg-0 ">
+          <li class="nav-item text-capitalize dropdown">
+            <span class="nav-link text-white fw-bold user dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             {{ Auth::user()->name}} {{ Auth::user()->surname }}</span>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown ">
+                <a class="dropdown-item drop-auth" href="{{ route('admin.home') }}">{{__('Dashboard')}}</a>
+                <a class="dropdown-item drop-auth" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                <a class="dropdown-item drop-auth" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+
           </li>
 
           <li class="nav-item">
@@ -32,5 +46,6 @@
       </div>
     </div>
   </nav>
+
 
 </header>
