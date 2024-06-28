c0068996e5604996d2eabb1d6ef7f7eb00922ce0
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-06-28 12:46:46 +0200

    [fix] payments layout

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -1,90 +1,99 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center py-5">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">PAGAMENTO</div>
-                    <div class="card-body">
-                        <p class="@if (session('success')) d-none @endif text-capitalize"> Sponsor <strong
-                                class="text-capitalize">{{ $sponsor?->name }}</strong> per
-                            <strong>{{ $house?->title }}</strong>
-                        </p>
-                        <p class="@if (session('success')) d-none @endif"><strong>User:</strong> {{ Auth::user()->name}} {{ Auth::user()->surname }}</p>
-                        <p class="@if (session('success')) d-none @endif"><strong>Ricevuta di pagamento inviata a:</strong> {{ Auth::user()->email}}</p>
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
+  <div class="container">
+    <div class="row justify-content-center py-5">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">PAGAMENTO</div>
+          <div class="card-body">
 
-                        @if (session('error'))
-                            <div class="alert alert-danger">
-                                {{ session('error') }}
-                            </div>
-                        @endif
+            @if (session('success'))
+              <div class="alert alert-success">
+                {{ session('success') }}
+              </div>
+            @endif
 
-                        <form method="post" action="{{ route('admin.payment.create') }}"
-                            class="@if (session('success')) d-none @endif">
-                            @csrf
+            @if (session('error'))
+              <div class="alert alert-danger">
+                {{ session('error') }}
+              </div>
+            @endif
 
-                            <div class="form-group">
-                                <label for="amount" class="fw-bold mb-2">Pagherai:</label>
-                                <input type="" class="border-0 text-dark bg-white" id="amount" name="amount"
-                                    value="{{ $sponsor?->price }} &euro;" readonly disabled>
-                            </div>
+            <p class="@if (session('success')) d-none @endif"><strong>Nome e Cognome:</strong>
+              {{ Auth::user()->name }}
+              {{ Auth::user()->surname }}</p>
+            <p class="@if (session('success')) d-none @endif"><strong>Ricevuta di pagamento a:</strong>
+              {{ Auth::user()->email }}</p>
 
-                            <div class="form-group">
-                                <label for="nonce"></label>
-                                <div id="dropin-container"></div>
-                                <input type="hidden" id="nonce" name="payment_method_nonce">
-                            </div>
+            <p class="@if (session('success')) d-none @endif text-capitalize"> Sponsor <strong
+                class="text-capitalize">{{ $sponsor?->name }}</strong> per
+              <strong>{{ $house?->title }}</strong>
+            </p>
 
-                            <button type="submit" class="btn btn-primary">Paga</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <form method="post" action="{{ route('admin.payment.create') }}"
+              class="@if (session('success')) d-none @endif">
+              @csrf
+
+              <div class="form-group">
+                <label for="amount" class="fw-bold mb-2">Pagherai:</label>
+                <input type="" class="border-0 text-dark bg-white" id="amount" name="amount"
+                  value="{{ $sponsor?->price }} &euro;" readonly disabled>
+              </div>
+
+              <div class="form-group">
+                <label for="nonce"></label>
+                <div id="dropin-container"></div>
+                <input type="hidden" id="nonce" name="payment_method_nonce">
+              </div>
+
+              <button type="submit" class="btn btn-primary" id="pay-button" disabled>Paga</button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 
-    <script src="https://js.braintreegateway.com/web/dropin/1.35.0/js/dropin.min.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var form = document.querySelector('form');
+  <script src="https://js.braintreegateway.com/web/dropin/1.35.0/js/dropin.min.js"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var form = document.querySelector('form');
+      var payButton = document.getElementById('pay-button');
 
-            fetch('{{ route('admin.payment.token') }}')
-                .then(response => response.json())
-                .then(data => {
-                    var clientToken = data.clientToken;
+      fetch('{{ route('admin.payment.token') }}')
+        .then(response => response.json())
+        .then(data => {
+          var clientToken = data.clientToken;
 
-                    braintree.dropin.create({
-                        authorization: clientToken,
-                        container: '#dropin-container'
-                    }, function(createErr, instance) {
-                        if (createErr) {
-                            console.error('Error creating Braintree Drop-in:', createErr);
-                            return;
-                        }
+          braintree.dropin.create({
+            authorization: clientToken,
+            container: '#dropin-container'
+          }, function(createErr, instance) {
+            if (createErr) {
+              console.error('Error creating Braintree Drop-in:', createErr);
+              return;
+            }
 
-                        form.addEventListener('submit', function(event) {
-                            event.preventDefault();
+            payButton.disabled = false;
 
-                            instance.requestPaymentMethod(function(err, payload) {
-                                if (err) {
-                                    console.error('Error requesting payment method:',
-                                        err);
-                                    return;
-                                }
+            form.addEventListener('submit', function(event) {
+              event.preventDefault();
 
-                                document.querySelector('#nonce').value = payload.nonce;
-                                form.submit();
-                            });
-                        });
-                    });
-                })
-                .catch(error => console.error('Error fetching client token:', error));
-        });
-    </script>
+              instance.requestPaymentMethod(function(err, payload) {
+                if (err) {
+                  console.error('Error requesting payment method:',
+                    err);
+                  return;
+                }
+
+                document.querySelector('#nonce').value = payload.nonce;
+                form.submit();
+              });
+            });
+          });
+        })
+        .catch(error => console.error('Error fetching client token:', error));
+    });
+  </script>
 @endsection
