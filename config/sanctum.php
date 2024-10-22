08c755dbc4618fc9fe8b5f4489c1460a93e164ec
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-10-22 14:34:31 +0200

    [fix] sancturm.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,https://vue-boolbnb.vercel.app',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,https://vue-boolbnb.vercel.app,https://laravel-boolbnb-production.up.railway.app',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
